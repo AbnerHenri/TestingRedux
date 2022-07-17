e470ab012b4c1275f4486a75968ddd5533df5764
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-17 17:28:47 -0300

    Create Structure the Redux

---
+++ redux/src/Reducers/CountReducers.js
@@ -0,0 +1,18 @@
+
+
+function CountReducer(state = 0, action){
+    switch (action.type) {
+        case 'INCREMENT':
+            
+            break;
+
+        case "DECREMENT":
+
+            break;
+    
+        default:
+            break;
+    }
+}
+
+export default CountReducer;
