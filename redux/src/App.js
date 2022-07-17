e03eb575e6aa02689230cc87ba5ec7f95214896c
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-17 18:36:31 -0300

    Recreate Initial Commit

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 
-import PrincipalCount from './Components/Principal/PrincipalCount';
-import SecondaryCount from './Components/Secondary/SecondaryCount';
-
 function App() {
   return (
     <div className="App">
-      <PrincipalCount />
-      <SecondaryCount />
+     
     </div>
   );
 }
--- redux/src/index.js
+++ redux/src/index.js
@@ -3,21 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-
-import CountReducer from './Reducers/CountReducers';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const Store = createStore(CountReducer)
-
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
-      <App />
-    </Provider>   
+    <App />
   </React.StrictMode>
 );
-
 
