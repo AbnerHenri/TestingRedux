d12df8ce3acae3895829da2f10f203aa1047ff71
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-17 17:15:43 -0300

    Create Structure

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import PrincipalCount from './Components/PrincipalCount';
+import SecondaryCount from './Components/SecondaryCount';
+
 function App() {
   return (
     <div className="App">
-    
+      <PrincipalCount />
+      <SecondaryCount />
     </div>
   );
 }
---
+++ redux/src/Components/SecondaryCount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function SecondaryCount() {
+  return(
+    <div>
+
+    </div>
+  );
+}
+
+export default SecondaryCount;
