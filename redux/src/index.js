00c9639c2e1ce78f1401f50cfe0e036fc9ebde71
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-17 18:48:07 -0300

    Create Redux Structure

--- redux/src/index.js
+++ redux/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Count } from './Reducers/Reducers'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Store = createStore(Count)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
