0c2645962151f097b270259f75e6199939a36ea9
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-07-18 12:56:56 -0300

    Add Feature Dec and Inc

--- redux/src/Components/Principal/PrincipalCount.js
+++ redux/src/Components/Principal/PrincipalCount.js
@@ -3,17 +3,27 @@ import './Principal.css'
 
 import Arrow from '../../Assets/arrow.png'
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../../Redux/CounterSlice'
 
 function Principal() {
 
   const Count = useSelector((state)=> { return state.count.value })
+  const Dispatch = useDispatch()
+
+  function changeState(type) {
+    if(type === 'INCREMENT'){
+      Dispatch(increment())
+    }else{
+      Dispatch(decrement())
+    }
+  }
 
   return(
     <div className='Principal'>
-      <img src={Arrow} className='Image Up' />
+      <img src={Arrow} className='Image Up' alt='Increment' onClick={()=> changeState('INCREMENT')}/>
         <h2>{Count}</h2>
-      <img src={Arrow} className='Image Down' />
+      <img src={Arrow} className='Image Down' alt='Decrement' onClick={()=> changeState('DECREMENT')}/>
     </div>
   );
 }
